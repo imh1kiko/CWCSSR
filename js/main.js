c8cf3ed3b58da0660c1b232bba77aacf5b5798cf
Hiki Hollow <imh1kiko@gmail.com>
2019-03-03 22:37:23 +0200

    Profile page update

---
+++ js/main.js
@@ -0,0 +1,83 @@
+/*
+ * Author: Hiki
+ * Last Modified: 03-03-2019 22:36:59
+ * 
+ * Status: D I S C A R D E D
+ * Reason: Cannot affect Iframe.
+ */
+
+ // !! You can use the first line tho. Just gets rid of some annoyance !! //
+$("#oh-mainmenu").remove();
+
+
+$(".with-apps.multisubmit #output>.submit-tabbar,.with-apps.multisubmit #output>.submit-tabbar>.submit-tabbar-overlay ").css({
+    "top": "0"
+});
+
+$("#output>.submit-tabbar").css({
+    "background": "var(--mid)"
+});
+
+$("html body").css({
+    "background": "none !important"
+});
+
+$("body.no-overhead, #dv7, textarea, input[type='text'], input[type='password'], select, #ile-contents input.text, #ile-contents textarea.text").css({
+    "background": "none"
+});
+
+$("html, body div#output, body div#output #dv7, html, body div#output, body div#output #dv7, body.no-overhead #overhead").css({
+    "background-color": "none"
+});
+
+$("body.no-overhead #overhead").css({
+    "background-color": "#342554 !important"
+});
+
+$(".ile-step-zero-notice").css({
+    "color": "white"
+});
+
+$("#ile-container, #autocomplete-placebo, #output #ile-contents .bubbleview").css({
+    "background": "#221a35"
+});
+
+$("#ile-contents .ile-description .ile-description-writer").css({
+    "border": "1px solid #342554",
+    "box-shadow": "0 0 3px 2px #342554",
+    "background": "#1e1a27"
+});
+
+$("#ile-contents .ile-title input.ile-title-input.default-text, #ile-contents .ile-description textarea.ile-description-input.default-text, #ile-contents .ile-keywords input.ile-keywords-input.default-text.text.small").css({
+    "background": "#75418c57"
+});
+
+$("#ile-contents input.ile-title-input").css({
+    "border": "1px solid #342554",
+    "box-shadow": "0 0 3px 2px #342554",
+    "border-radius": "15px 15px 0 0",
+    "padding-left": "20px"
+})
+
+$(".stash-upload-box").css({
+    "box-shadow": "none",
+    "background": "#1e1a27",
+    "border": "1px solid #75418c",
+    "color": "#fff"
+});
+
+$(".multisubmit .iframed_submitform").css({
+    "margin-top": "5px"
+});
+
+$("body.deviantart #overhead").css({
+    "background-color": "#342554"
+});
+
+$("#ile-container, #autocomplete-placebo, #output #ile-contents .bubbleview").css({
+    "background-color": "#221a35"
+});
+
+$(".multisubmit .iframed_submitform").css({
+    "margin-top":"5px"
+});
